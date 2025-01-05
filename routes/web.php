7795969acd127bbd2855f3db0c674f020271bdf9
hurryzyn <harry.muzayin@gmail.com>
2025-01-05 12:57:39 +0700

    Add home page layout and routing, including product display and footer

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/detail_product', function () {
+    return view('cust.product.detail_product');
+});
+Route::get('/cust/login', function () {
+    return view('cust.login.login');
+})->name('login');
+
+
